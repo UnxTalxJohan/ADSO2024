d22716ff42bce2f1b144daab8aae96184cf640a9
Johan Amaya <Johamadic11@gmail.com>
2024-08-22 09:42:30 -0500

    Nuevo Repositorio

---
+++ ciclos.py
@@ -0,0 +1,9 @@
+
+
+# Utilizamos la función `range(1, 11)` para generar una secuencia de números del 1 al 10.
+for numero in range(1, 11):
+    # Calculamos el cuadrado del número actual.
+    cuadrado = numero ** 2
+    
+    # Imprimimos el número actual y su cuadrado.
+    print(f"El número {numero} tiene como cuadrado {cuadrado}.")
